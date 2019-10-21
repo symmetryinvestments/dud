b8b96159da26d42f61e11bb5d8d8c87503be5264
Robert Schadek <rschadek@symmetryinvestments.com>
2019-10-21 16:56:10 +0100

    starting to learn sdl

--- pkgdescription/source/dud/pkgdescription/sdl.d
+++ pkgdescription/source/dud/pkgdescription/sdl.d
@@ -7,10 +7,20 @@ import dud.path : Path;
 import dud.sdlang;
 
 PackageDescription sdlToPackageDescription(string sdl) {
-	Tag jv = parseFile(sdl);
+	Tag jv = parseSource(sdl);
 	return sdlToPackageDescription(jv);
 }
 
 PackageDescription sdlToPackageDescription(Tag t) {
+	import std.stdio;
+	writeln("Attributes");
+	foreach(it; t.attributes()) {
+		writeln(it);
+	}
+
+	writeln("Tags");
+	foreach(it; t.tags()) {
+		writeln(it);
+	}
 	return PackageDescription.init;
 }
