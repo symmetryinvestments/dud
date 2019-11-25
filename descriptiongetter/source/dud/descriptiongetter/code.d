c490cbb416bcfa8aed62a3a66e81639fb81472b1
Robert Schadek <rschadek@symmetryinvestments.com>
2019-11-25 11:33:14 +0000

    descriptiongetter

---
+++ descriptiongetter/source/dud/descriptiongetter/code.d
@@ -0,0 +1,55 @@
+module dud.descriptiongetter.code;
+
+import std.array : array;
+import std.exception : enforce;
+import std.algorithm.iteration : each, filter, map;
+import std.algorithm.searching : canFind;
+import std.json;
+
+@safe:
+
+JSONValue getCodeDlangDump() @trusted {
+	import std.exception : assumeUnique;
+	import std.net.curl;
+	import std.zlib;
+
+	auto data = get("https://code.dlang.org/api/packages/dump");
+
+	auto uc = new UnCompress();
+
+	const(void[]) un = uc.uncompress(data);
+	return parseJSON(cast(const(char)[])un);
+}
+
+JSONValue trimCodeDlangDump(JSONValue old) {
+	if(old.type == JSONType.array) {
+		return trimArray(old.arrayNoRef());
+	} else if(old.type == JSONType.object) {
+		return trimObject(old);
+	} else {
+		return old;
+	}
+}
+
+JSONValue trimArray(JSONValue[] old) {
+	return JSONValue(old.map!(it => trimCodeDlangDump(it)).array);
+}
+
+JSONValue trimObject(JSONValue obj) {
+	enforce(obj.type == JSONType.object);
+	JSONValue ret;
+	foreach(key, value; obj.objectNoRef()) {
+		if(key == "info") {
+			ret["packageDescription"] = value;
+		} else if(!canFind(
+			[ "readme", "readmeMarkdown", "docFolder"
+			, "packageDescriptionFile", "logo", "errors"
+			, "categories", "owner", "errors", "stats", "textScore"
+			, "updateCounter", "_id", "updatedAt", "commitID", "date"
+			], key))
+		{
+			ret[key] = trimCodeDlangDump(value);
+		}
+	}
+	return ret;
+}
--- testdata/source/dud/testdata/main.d
+++ testdata/source/dud/testdata/main.d
@@ -1,5 +1,52 @@
 module dud.testdata.main;
 
-int main(string[] args) {
-	return 0;
+import std.array : empty;
+import std.json;
+import std.stdio;
+import std.getopt;
+import std.file : readText;
+
+import dud.descriptiongetter.code;
+
+private struct Options {
+	bool getCodeDump;
+	string outFilename;
+	string inFilename;
+}
+
+void main(string[] args) {
+	Options options;
+	auto helpWanted = getopt(args,
+		"g|getCodeDump", "Download the dump from the code.dlang.org api",
+		&options.getCodeDump,
+		"o|outFilename", "The filename of the output of the trimed dump",
+		&options.outFilename,
+		"i|inFilename", "In filename to a code.dlang.org dump.json file",
+		&options.inFilename
+		);
+
+	if(helpWanted.helpWanted) {
+		defaultGetoptPrinter("CLI to handle the code.dlang.org api dump",
+			helpWanted.options);
+		return;
+	}
+
+	JSONValue all = !options.outFilename.empty
+		? parseJSON(readText(options.outFilename))
+		: options.getCodeDump
+			? getCodeDlangDump()
+			: JSONValue.init;
+
+	if(all.type == JSONType.null_) {
+		return;
+	}
+
+	JSONValue shorter = trimCodeDlangDump(all);
+
+	if(!options.inFilename.empty) {
+		auto f = File(options.inFilename, "w");
+		f.writeln(shorter.toPrettyString());
+	} else {
+		writeln(shorter.toPrettyString());
+	}
 }
