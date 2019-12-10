7a321350b268f37663c17ec1b386d7c867ab6d92
Robert Schadek <rschadek@symmetryinvestments.com>
2019-12-10 12:29:19 +0000

    some first tests

--- semver2/source/dud/semver2/versionunion.d
+++ semver2/source/dud/semver2/versionunion.d
@@ -2,14 +2,17 @@ module dud.semver2.versionunion;
 
 import std.format : format;
 import dud.semver2.versionrange;
+import dud.semver2.semver;
+
+@safe pure:
 
 struct VersionUnion {
 @safe pure:
 	VersionRange[] ranges;
 
-	void insert(VersionRange vr) {
+	/*void insert(VersionRange vr) {
 		const SetRelation sr = relation
-	}
+	}*/
 }
 
 VersionRange merge(const(VersionRange) a, const(VersionRange) b) {
@@ -17,10 +20,10 @@ VersionRange merge(const(VersionRange) a, const(VersionRange) b) {
 		return VersionRange(a.low, a.inclusiveLow, b.high, b.inclusiveHigh);
 	} else if(a.low == b.high && a.inclusiveLow && b.inclusiveHigh) {
 		return VersionRange(b.low, b.inclusiveLow, a.high, a.inclusiveHigh);
-	} if(a.low <= b.high && a.low >= b.low) {
+	} if(a.low < b.high && a.low > b.low) {
 		return VersionRange(b.low, b.inclusiveLow, a.high, a.inclusiveHigh);
-	} else if(b.low <= a.high && b.low >= a.low) {
+	} else if(b.low < a.high && b.low > a.low) {
 		return VersionRange(a.low, a.inclusiveLow, b.high, b.inclusiveHigh);
 	}
-	assert(false, format("\na: %s\nb: %s can not be merged", a, b));
+	return VersionRange(SemVer.init, Inclusive.no, SemVer.init, Inclusive.no);
 }
--- semver2/source/dud/semver2/versionuniontest.d
+++ semver2/source/dud/semver2/versionuniontest.d
@@ -6,4 +6,38 @@ import dud.semver2.semver;
 import dud.semver2.parse;
 import dud.semver2.versionrange;
 import dud.semver2.versionunion;
+import dud.semver2.comparision;
 
+unittest {
+	VersionRange vr1 = parseVersionRange(">=1.0.0 <=2.0.0").get();
+	VersionRange vr2 = parseVersionRange(">=1.5.0 <=3.0.0").get();
+
+	VersionRange m = merge(vr1, vr2);
+	SetRelation sr = relation(m, parseVersionRange(">=1.0.0 <=3.0.0").get());
+	assert(sr == SetRelation.subset, format("\nm: %s\nsr: %s", m, sr));
+
+	m = merge(vr2, vr1);
+	sr = relation(m, parseVersionRange(">=1.0.0 <=3.0.0").get());
+	assert(sr == SetRelation.subset, format("\nm: %s\nsr: %s", m, sr));
+}
+
+unittest {
+	VersionRange vr1 = parseVersionRange(">=1.0.0 <=1.5.0").get();
+	VersionRange vr2 = parseVersionRange(">=1.5.0 <=3.0.0").get();
+
+	VersionRange m = merge(vr1, vr2);
+	SetRelation sr = relation(m, parseVersionRange(">=1.0.0 <=3.0.0").get());
+	assert(sr == SetRelation.subset, format("\nm: %s\nsr: %s", m, sr));
+
+	m = merge(vr2, vr1);
+	sr = relation(m, parseVersionRange(">=1.0.0 <=3.0.0").get());
+	assert(sr == SetRelation.subset, format("\nm: %s\nsr: %s", m, sr));
+}
+
+unittest {
+	VersionRange vr1 = parseVersionRange(">=1.0.0 <1.5.0").get();
+	VersionRange vr2 = parseVersionRange(">1.5.0 <=3.0.0").get();
+
+	VersionRange m = merge(vr1, vr2);
+	assert(m == VersionRange.init, format("\n%s", m));
+}
