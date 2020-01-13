131eba4b2af0230a84ad82c050679bbfa0662f0d
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2020-01-13 15:42:36 +0000

    somemoreconst

--- semver2/source/dud/semver2/checks.d
+++ semver2/source/dud/semver2/checks.d
@@ -5,30 +5,33 @@ import dud.semver2.range;
 import dud.semver2.semver;
 
 /// Returns `true` if this constraint allows [version].
-bool allows(VersionRange toCheckIn, SemVer toCheck);
-bool allows(VersionRange toCheckIn, VersionRange toCheck);
-bool allows(VersionRange toCheckIn, VersionUnion toCheck);
+bool allows(const(VersionRange) toCheckIn, const(SemVer) toCheck) {
+	return isInRange(toCheckIn, toCheck);
+}
 
-bool allows(VersionUnion toCheckIn, SemVer toCheck);
-bool allows(VersionUnion toCheckIn, VersionRange toCheck);
-bool allows(VersionUnion toCheckIn, VersionUnion toCheck);
+bool allows(const(VersionRange) toCheckIn, const(VersionRange) toCheck);
+bool allows(const(VersionRange) toCheckIn, const(VersionUnion) toCheck);
+
+bool allows(const(VersionUnion) toCheckIn, const(SemVer) toCheck);
+bool allows(const(VersionUnion) toCheckIn, const(VersionRange) toCheck);
+bool allows(const(VersionUnion) toCheckIn, const(VersionUnion) toCheck);
 
 /// Returns `true` if this constraint allows all the versions that [other]
 /// allows.
-bool allowsAll(VersionRange toCheckIn, SemVer toCheck);
-bool allowsAll(VersionRange toCheckIn, VersionRange toCheck);
-bool allowsAll(VersionRange toCheckIn, VersionUnion toCheck);
+bool allowsAll(const(VersionRange) toCheckIn, const(SemVer) toCheck);
+bool allowsAll(const(VersionRange) toCheckIn, const(VersionRange) toCheck);
+bool allowsAll(const(VersionRange) toCheckIn, const(VersionUnion) toCheck);
 
-bool allowsAll(VersionUnion toCheckIn, SemVer toCheck);
-bool allowsAll(VersionUnion toCheckIn, VersionRange toCheck);
-bool allowsAll(VersionUnion toCheckIn, VersionUnion toCheck);
+bool allowsAll(const(VersionUnion) toCheckIn, const(SemVer) toCheck);
+bool allowsAll(const(VersionUnion) toCheckIn, const(VersionRange) toCheck);
+bool allowsAll(const(VersionUnion) toCheckIn, const(VersionUnion) toCheck);
 
 /// Returns `true` if this constraint allows any of the versions that [other]
 /// allows.
-bool allowsAny(VersionRange toCheckIn, SemVer toCheck);
-bool allowsAny(VersionRange toCheckIn, VersionRange toCheck);
-bool allowsAny(VersionRange toCheckIn, VersionUnion toCheck);
+bool allowsAny(const(VersionRange) toCheckIn, const(SemVer) toCheck);
+bool allowsAny(const(VersionRange) toCheckIn, const(VersionRange) toCheck);
+bool allowsAny(const(VersionRange) toCheckIn, const(VersionUnion) toCheck);
 
-bool allowsAny(VersionUnion toCheckIn, SemVer toCheck);
-bool allowsAny(VersionUnion toCheckIn, VersionRange toCheck);
-bool allowsAny(VersionUnion toCheckIn, VersionUnion toCheck);
+bool allowsAny(const(VersionUnion) toCheckIn, const(SemVer) toCheck);
+bool allowsAny(const(VersionUnion) toCheckIn, const(VersionRange) toCheck);
+bool allowsAny(const(VersionUnion) toCheckIn, const(VersionUnion) toCheck);
