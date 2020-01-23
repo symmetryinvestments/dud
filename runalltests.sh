100ff560c717b52736620b3785d47c3d38d98a00
Robert Schadek <rschadek@symmetryinvestments.com>
2020-01-23 12:32:48 +0000

    better testing

---
+++ runalltests.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/bash
+
+function unittest() {
+	dub test --compiler=$1
+	return $?
+}
+
+function buildConf() {
+	dub --compiler=$1 --config=$2
+	return $?
+}
+
+function test() {
+	echo "TESTING: $1 with $2"
+	cd $1
+	unittest $2
+	local e=$?
+	if [[ $e -ne 0 ]]
+	then
+		cd ..
+		echo "FAILED: $1 unittest with compiler $2 with error code $e"
+		exit $e
+	fi
+
+	for i in ${@:3} ;
+	do
+		buildConf $2 $i
+		local f=$?
+		if [[ $f -ne 0 ]]
+		then
+			cd ..
+			echo "FAILED: $1 with compiler $2 and configuration $i, error code $f"
+			exit $f
+		fi
+	done
+	cd ..
+	return 0
+}
+
+for dc in $1
+do
+	test utils ${dc}
+	test exception ${dc}
+	test sdlang ${dc} "ExcessiveTests"
+	test semver ${dc}
+	#test pkgdescription ${dc} "ExcessiveSDLTests" "ExcessiveJSONTests" "ExcessiveConvTests"
+	test pkgdescription ${dc}
+	test resolve ${dc}
+	test testdata ${dc} "app"
+	test descriptiongetter ${dc}
+done
+exit 0
