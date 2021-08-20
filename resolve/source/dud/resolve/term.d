6efc3bc978c3677a9f9cbfe0e1b98d8b0d979606
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2021-08-20 14:41:30 +0200

    using what is there to invert Terms

--- resolve/source/dud/resolve/term.d
+++ resolve/source/dud/resolve/term.d
@@ -1,6 +1,8 @@
 module dud.resolve.term;
 
 import std.exception : enforce;
+import std.typecons : Nullable, nullable;
+debug import std.stdio;
 
 import dud.pkgdescription;
 import dud.semver.versionrange : SetRelation;
@@ -13,11 +15,18 @@ import dud.resolve.confs;
 
 @safe pure:
 struct Term {
+@safe pure:
 	VersionConfigurationToolchain constraint;
 	PackageDescriptionVersionRange pkg;
 
 	/// is only an indicator `contraint` is used to store both stats
 	IsPositive isPositive;
+
+	string toString() const {
+		import std.format : format;
+		return format("Term(%s, %s, %s)", this.pkg.pkg.name
+				, this.isPositive, this.constraint);
+	}
 }
 
 Term invert(const(Term) t) {
@@ -30,9 +39,35 @@ bool satisfies(const(Term) that, const(Term) other) {
 		&& relation(other, that) == SetRelation.subset;
 }
 
-SetRelation relation(const(Term) a, const(Term) b) {
-	static import dud.resolve.versionconfigurationtoolchain;
-	enforce(a.pkg.pkg.name == b.pkg.pkg.name);
-	return dud.resolve.versionconfigurationtoolchain.relation(
-			a.constraint, b.constraint);
+SetRelation relation(const(Term) that, const(Term) other) {
+	enforce(that.pkg.pkg.name == other.pkg.pkg.name);
+
+	debug writefln("that: %3s other: %3s", that.isPositive, other.isPositive);
+	const(Term) that2 = that.isPositive
+		? that
+		: that.invert();
+
+	const(Term) other2 = other.isPositive
+		? other
+		: other.invert();
+
+	return dud.resolve.versionconfigurationtoolchain.relation(that2.constraint
+			, other2.constraint);
+}
+
+Nullable!(Term) intersectionOf(const(Term) a, const(Term) b) {
+	const(VersionConfigurationToolchain) a2 = a.isPositive
+		? a.constraint
+		: dud.resolve.versionconfigurationtoolchain.invert(a.constraint);
+
+	const(VersionConfigurationToolchain) b2 = b.isPositive
+		? b.constraint
+		: dud.resolve.versionconfigurationtoolchain.invert(b.constraint);
+
+	VersionConfigurationToolchain r = dud.resolve.versionconfigurationtoolchain
+		.intersectionOf(a2, b2);
+
+	return r.conf.confs.empty || r.ver.ranges.empty
+		? Nullable!(Term).init
+		: nullable(Term(r, a.pkg, IsPositive.yes));
 }
--- resolve/source/dud/resolve/termtest.d
+++ resolve/source/dud/resolve/termtest.d
@@ -1,16 +1,17 @@
 module dud.resolve.termtest;
 
-import dud.resolve.term;
-import dud.resolve.positive;
-import dud.resolve.confs;
 import dud.resolve.conf;
+import dud.resolve.confs;
+import dud.resolve.positive;
+import dud.resolve.term;
 import dud.resolve.versionconfigurationtoolchain : VersionConfigurationToolchain;
+import dud.semver.checks;
+import dud.semver.versionrange;
+import dud.semver.versionunion;
+
+@safe pure:
 
 unittest {
-	import dud.semver.versionrange;
-	import dud.semver.versionunion;
-	import dud.semver.checks;
-
 	Term t1;
 	t1.pkg.ver = parseVersionRange("1.2.3").get();
 	t1.isPositive = IsPositive.yes;
@@ -19,6 +20,59 @@ unittest {
 			Confs([Conf("", IsPositive.yes)])
 		);
 
+	assert(satisfies(t1, t1));
+
 	Term t2 = t1.invert();
 	assert(!allowsAny(t1.constraint.ver, t2.constraint.ver));
+	assert(!satisfies(t1, t2));
+}
+
+private void testRelation(const(Term) a, const(Term) b, const(SetRelation) exp
+		, int line = __LINE__)
+{
+	import core.exception : AssertError;
+	import std.exception : enforce;
+	import std.format : format;
+
+	const(SetRelation) rslt = relation(a, b);
+	enforce!AssertError(rslt == exp,
+		format("\na: %s\nb: %s\nexp: %s\nrsl: %s", a, b, exp, rslt),
+		__FILE__, line);
+
+}
+
+unittest {
+	Term t1;
+	t1.isPositive = IsPositive.yes;
+	t1.constraint = VersionConfigurationToolchain(
+			VersionUnion([ parseVersionRange(">=1.0.0").get() ]),
+			Confs([Conf("", IsPositive.yes)])
+		);
+
+	Term t2;
+	t2.isPositive = IsPositive.no;
+	t2.constraint = VersionConfigurationToolchain(
+			VersionUnion([ parseVersionRange(">=1.0.0").get() ]),
+			Confs([Conf("", IsPositive.yes)])
+		);
+
+	testRelation(t1, t2, SetRelation.disjoint);
+}
+
+unittest {
+	Term t1;
+	t1.isPositive = IsPositive.yes;
+	t1.constraint = VersionConfigurationToolchain(
+			VersionUnion([ parseVersionRange("1.0.0").get() ]),
+			Confs([Conf("", IsPositive.yes)])
+		);
+
+	Term t2;
+	t2.isPositive = IsPositive.no;
+	t2.constraint = VersionConfigurationToolchain(
+			VersionUnion([ parseVersionRange("<1.0.0").get() ]),
+			Confs([Conf("", IsPositive.yes)])
+		);
+
+	testRelation(t1, t2, SetRelation.disjoint);
 }
