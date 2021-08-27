9421f29d459d620c276f18eff042da5f9f3e4460
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2021-08-27 15:18:55 +0200

    starting on the Incompatibilities of pubgrub
    to get a feeling for the actual algorithm

--- resolve/source/dud/resolve/providiertest.d
+++ resolve/source/dud/resolve/providiertest.d
@@ -5,7 +5,7 @@ import std.stdio;
 import dud.resolve.providier;
 
 unittest {
-	auto dfp = DumpFileProvidier("../testdata/dump_short.json");
+	auto dfp = fromDumpFile("../testdata/dump_short.json");
 
 	//auto graphqld = dfp.getPackages("graphqld", "^1.0.0");
 	//writeln(graphqld);
